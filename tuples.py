84f71fc3f7627e0ec67d2c554d2220ced469cb67
Nihal5014 <Nihal23.banda@gmail.com>
2024-10-27 18:46:50 +0000

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,23 @@
+numbers =  (1,2,3,4,5,6,7,8,9)#tuple - data structure
+print(numbers)
+print(numbers[4])
+
+#numbers.remove(8)
+#print(numbers)
+# You cant modify the tuple.
+
+n =1,2,3
+print(n)
+#packing of tuple
+
+a,b,c = n
+print(a)          
+print(b)
+print(c)
+#unpacking of the tuple
+
+print(numbers[3:6])#slicing on tuple
+
+for i in numbers:
+    print(i)
+print(numbers[3:6])
